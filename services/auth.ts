1d1fab0345aedffcd35c90246bd007107a65191f
loraine <mukezwa@gmail.com>
2022-05-16 17:11:36 +0200

    fix: import on signup

--- services/auth.ts
+++ services/auth.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
-import { LoginResponse, Response, SignIn, SignUp } from '../types';
+import { LoginResponse, Response, SignIn, Signup } from '../types';
 
 const url = 'http://196.223.240.154:8099/supapp/api';
 
-export async function signup(signUpInfo: SignUp): Promise<Response<void>> {
+export async function signup(signUpInfo: Signup): Promise<Response<void>> {
   return await axios.post(`${url}/auth/client/signup`, signUpInfo);
 }
 
